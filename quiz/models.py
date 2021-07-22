db56307b994be3cea601923f673b33644626d08f
hibeen1 <hibeen1@gmail.com>
2021-07-23 03:59:14 +0900

    admin 모델 ui 변경

--- quiz/models.py
+++ quiz/models.py
@@ -5,3 +5,6 @@ class Quiz(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     answer = models.IntegerField()
+
+    def __str__(self):
+        return self.title
